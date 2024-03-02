32cbb62c69a0a74a1f968b513780a4e9f1b19dfa
Krishna <krishna.sk7991@gmail.com>
2024-03-03 00:53:22 +0530

    java in vs code setup

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Demo {
+
+     public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println("X : "+x);
+        sc.close();
+     }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+## How to setup code,input and output on split panes in vs code
+### source :  [youtube](https://www.youtube.com/watch?v=h3uDCJ5mvgw&ab_channel=takeUforward), [website](https://takeuforward.org/set-up/how-to-set-up-visual-studio-code-for-java-programming-cp-and-dsa/)
